6a5e834ce75a7f5bac2ab162e2294c7b2e1c9adc
NeroMilk <mingfung@efun.com>
2018-08-11 23:46:25 +0800

    尝试移除回.build内容。继续解决Heroku的Application Error的问题

--- .build/x86_64-apple-macosx10.10/debug/CNIOLinux.build/shim.c.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/neromilk/Documents/SavingThePlanet/.build/checkouts/swift-nio-zlib-support.git--1071467962839356487/module.modulemap \
-  /Users/neromilk/Documents/SavingThePlanet/.build/checkouts/swift-nio.git-3108475404973543938/Sources/CNIOLinux/shim.c
