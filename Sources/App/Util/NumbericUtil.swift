5afdff0c6eedb47fbd81e3233f52acf61f6fe473
NeroMilk <mingfung@efun.com>
2018-10-03 00:13:22 +0800

    fixed the problem about arc4random_uniform does not work on linux. https://stackoverflow.com/questions/40531549/vapor-error-h13-on-heroku

--- Sources/App/Util/NumbericUtil.swift
+++ Sources/App/Util/NumbericUtil.swift
@@ -6,6 +6,10 @@
 //
 
 import Foundation
+import Cocoa
+#if os(Linux)
+import Glibc
+#endif
 
 class NumbericUtil {
     static func randomNumbericString(length: Int) -> String {
@@ -13,10 +17,19 @@ class NumbericUtil {
         
         var result = ""
         for _ in 0..<length {
-            let index = Int(arc4random_uniform(UInt32(numbers.count)))
+            let index = randomInt(min: 0, max: numbers.count - 1)
             result.append(numbers[numbers.index(numbers.startIndex, offsetBy: index)])
         }
         return result
     }
+    
+    public static func randomInt(min: Int, max:Int) -> Int {
+        #if os(Linux)
+        return Glibc.random() % max
+        #else
+        return min + Int(arc4random_uniform(UInt32(max - min + 1)))
+        #endif
+    }
 }
+
 
